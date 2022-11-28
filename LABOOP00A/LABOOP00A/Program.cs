c2cc6ce5cf9d6c73ee1c528abc0452a9d195abae
prasitticha <phanuwat.sa@kkumail.com>
2022-11-28 15:40:25 +0700

    Add Commanline

---
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print string to commanline 
+Console.WriteLine("Phanuwat Sarawong");
+// id
+Console.WriteLine("653450295-8");
+// email
+Console.WriteLine("phanuwat.sa@kkumail.com");
